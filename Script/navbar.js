8d9c5a7b0e64eeed5ca0d316ecce8232715fad85
Syed Maaz Hassan <hafizmaazhassan33@gmail.com>
2022-08-12 22:47:58 -0700

    Add files via upload

---
+++ Script/navbar.js
@@ -0,0 +1,8 @@
+$('#services').hover(function () {
+    $('.service-drop').css({ 'visibility': 'visible' })
+})
+
+$('#home-banner').hover(function () {
+    $('.service-drop').css({ 'visibility': 'hidden' })
+
+})
